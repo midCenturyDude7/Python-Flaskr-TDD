b685a823328737133ec2c733559e41dcdef64987
midCenturyDude7 <mgriffes@gmail.com>
2022-01-01 22:36:38 -0800

    Adding routes for login and logout

--- project/app.py
+++ project/app.py
@@ -1,8 +1,12 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for
+# from flask.helpers import url_for
 
 # db configuration
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
 
 # create and initialize a new Flask app
 app = Flask(__name__)
@@ -45,6 +49,30 @@ def close_db(error):
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
